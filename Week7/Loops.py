de6100f80a1d3067f8ad12a54ae5bd59949c0593
Page Nathan (DC-MH/ENG1.13-NA) <Nathan.Page@boschrexroth-us.com>
2021-06-04 16:38:36 -0400

    Added while and for loops

---
+++ Week7/Loops.py
@@ -0,0 +1,12 @@
+# indefinite loop 
+
+strInput = ' '
+while strInput != 'exit':
+    strInput = input('Enter word: ')
+    print(strInput)
+
+# Definite loop
+for i in [5,4,3,2,1]:
+    print(i)
+
+print("Blast off!")
